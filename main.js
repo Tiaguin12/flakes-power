58469c493eda2921b1da45fb2ec609a5cdc74e09
Tiaguin12 <162718694+Tiaguin12@users.noreply.github.com>
2024-03-12 11:23:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://cdn2.unrealengine.com/fortnite-chapter-4-og-overview-page-key-art-bg-1920x1080-1fbc3a1c0297.jpg"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://t2.tudocdn.net/611216?w=1920"
+}
